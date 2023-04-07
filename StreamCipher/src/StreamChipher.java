ca776f9774243a2b5814959ae01580db58dcc609
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-07 23:33:44 +0200

    Read file in bytes

--- StreamCipher/src/StreamChipher.java
+++ StreamCipher/src/StreamChipher.java
@@ -1,8 +1,31 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+
 public class StreamChipher {
     public static void main(String[] args) {
         if (args.length != 3) {
             System.err.println("Invalid number of arguments, args: <key> <infile> <outfile>");
             System.exit(1);
+        }
+
+
+    }
+
+    private static byte[] getBytesFromFile(String file){
+        Path inputFilePath = Path.of(file);
+        if (!Files.exists(inputFilePath)) {
+            System.out.println("Error: input file does not exist.");
+            System.exit(1);
+        }
+
+        try {
+            FileInputStream input = new FileInputStream(file);
+            return input.readAllBytes();
+        } catch (Exception e) {
+            System.out.println("Could not read file");
+            return null;
         }
     }
 }
