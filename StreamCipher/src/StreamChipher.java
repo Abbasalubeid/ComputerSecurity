301d687268abe89bb68d85381016aee9ff15f9b3
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-07 17:30:16 +0200

    Error msg

--- StreamCipher/src/StreamChipher.java
+++ StreamCipher/src/StreamChipher.java
@@ -1,8 +1,7 @@
 public class StreamChipher {
     public static void main(String[] args) {
         if (args.length != 3) {
-            System.err.println("Error: invalid number of arguments.");
-            System.err.println("args: <key> <infile> <outfile>");
+            System.err.println("Invalid number of arguments, args: <key> <infile> <outfile>");
             System.exit(1);
         }
     }
