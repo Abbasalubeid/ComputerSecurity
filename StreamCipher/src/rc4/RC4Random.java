12472bc3e94e1285c23558ddaf2846f12372a9cd
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-10 15:58:58 +0200

    Generate k for the cipher

--- StreamCipher/src/rc4/RC4Random.java
+++ StreamCipher/src/rc4/RC4Random.java
@@ -3,6 +3,8 @@ import java.util.Random;
 
 public class RC4Random extends Random {
     private int[] s = new int[256];
+    private int i = 0;
+    private int j = 0;
 
     public RC4Random(byte[] key) {
         for (int k = 0; k < 256; k++) {
@@ -16,5 +18,16 @@ public class RC4Random extends Random {
             s[j] = temp;
         }
     }
+
+    public int next(int bits) {
+        i = (i + 1) % 256;
+        j = (j + s[i]) % 256;
+        int temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        int k = s[(s[i] + s[j]) % 256];
+        return k & ((1 << bits) - 1);
+    }
+
 
 }
