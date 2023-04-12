5d97fd961fba397d60a2fbdf66c07dc2a4fd2089
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-12 15:06:46 +0200

    Typo

--- StreamCipher/src/StreamCipher.java
+++ StreamCipher/src/StreamCipher.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class StreamChipher {
+public class StreamCipher {
     public static void main(String[] args) {
         if (args.length != 3) {
             System.err.println("Invalid number of arguments, args: <key> <infile> <outfile>");
