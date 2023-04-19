27df139f6354d48f7f69f6f00329c17179abddcb
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-19 12:21:08 +0200

    args instead of hard coded

--- One-way-hash/src/BitCounter.java
+++ One-way-hash/src/BitCounter.java
@@ -2,10 +2,10 @@ import java.math.BigInteger;
 
 public class BitCounter {
     public static void main(String[] args) {
-        String md5Hash1 = "440658b6d478ec92f7724c6287e53224";
-        String sha256Hash1 = "e577b1a4bd1f8e662e7d20f604e9e9f6f6f68acb8473f3537b900a60699c729b";
-        String md5Hash2 = "c53ebeec5d02f1f5a63314c069e2f970";
-        String sha256Hash2 = "07528c1d3139dae813186b203bf89ca7e3ec3a199f2ae0e8cf24c73ca021680e";
+        String md5Hash1 = args[0];
+        String md5Hash2 = args[1];
+        String sha256Hash1  = args[2];
+        String sha256Hash2 = args[3];
 
         BigInteger md5Bits1 = new BigInteger(md5Hash1, 16);
         BigInteger sha256Bits1 = new BigInteger(sha256Hash1, 16);
